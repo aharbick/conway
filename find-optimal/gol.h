4e0e54ab34daf3c308479f4b5fc661dab8896927
aharbick@aharbick.com <aharbick@aharbick.com>
2022-01-06 13:30:00 -0500

    Trying another idea

--- find-optimal/gol.h
+++ find-optimal/gol.h
@@ -58,19 +58,19 @@ void printPattern(ulong64 number) {
 pthread_mutex_t gMutex = PTHREAD_MUTEX_INITIALIZER;
 int gBestGenerations = 0;
 
-__device__ void add2(ulong64 a, ulong64 b, ulong64 &s0, ulong64 &s1) {
+__host__ __device__ void add2(ulong64 a, ulong64 b, ulong64 &s0, ulong64 &s1) {
   s0 = a ^ b ;
   s1 = a & b ;
 }
 
-__device__ void add3(ulong64 a, ulong64 b, ulong64 c, ulong64 &s0, ulong64 &s1) {
+__host__ __device__ void add3(ulong64 a, ulong64 b, ulong64 c, ulong64 &s0, ulong64 &s1) {
   ulong64 t0, t1, t2 ;
   add2(a, b, t0, t1) ;
   add2(t0, c, s0, t2) ;
   s1 = t1 ^ t2 ;
 }
 
-__device__ ulong64 computeNextGeneration(ulong64 a) {
+__host__ __device__ ulong64 computeNextGeneration(ulong64 a) {
   ulong64 s0, sh2, a0, a1, sll, slh ;
   add2((a & 0x7f7f7f7f7f7f7f7f)<<1, (a & 0xFEFEFEFEFEFEFEFE)>>1, s0, sh2) ;
   add2(s0, a, a0, a1) ;
@@ -81,7 +81,7 @@ __device__ ulong64 computeNextGeneration(ulong64 a) {
   return (x^y^sh2^slh)&((x|y)^(sh2|slh))&(sll|a) ;
 }
 
-__device__ int countGenerations(ulong64 pattern) {
+__host__ __device__ int countGenerations(ulong64 pattern) {
   bool ended = false;
   int generations = 0;
 
@@ -142,79 +142,41 @@ __device__ int countGenerations(ulong64 pattern) {
   return ended ? generations : 0;
 }
 
-__device__ int countNextSix(ulong64 pattern) {
-  int generations = 6;
+#ifdef __NVCC__
+__global__ void findPromisingPatterns(ulong64 beginAt, ulong64 endAt, ulong64 *promising, ulong64 *numPromising) {
+  ulong64 pattern = beginAt + (blockIdx.x * blockDim.x + threadIdx.x);
   ulong64 g1, g2, g3, g4, g5, g6;
   g1 = pattern;
-  g2 = computeNextGeneration(g1);
-  g3 = computeNextGeneration(g2);
-  g4 = computeNextGeneration(g3);
-  g5 = computeNextGeneration(g4);
-  g6 = computeNextGeneration(g5);
-  g1 = computeNextGeneration(g6);
+  int generations = 0;
+  while (pattern < endAt) {
+    generations += 6;
+    g2 = computeNextGeneration(g1);
+    g3 = computeNextGeneration(g2);
+    g4 = computeNextGeneration(g3);
+    g5 = computeNextGeneration(g4);
+    g6 = computeNextGeneration(g5);
+    g1 = computeNextGeneration(g6);
 
-  if (g1 == 0) {
-    // Pattern ended Adjust the age
-    if (g2 == 0) {generations-=5;}
-    else if (g3 == 0) {generations-=4;}
-    else if (g4 == 0) {generations-=3;}
-    else if (g5 == 0) {generations-=2;}
-    else if (g6 == 0) {generations-=1;}
-  }
+    if (g1 == 0 || (g1 == g2) || (g1 == g3) || (g1 == g4)) {
+      // pattern is ended or is cyclical... reset counter ready to advance to next pattern.
+      generations = 0;
+    } else if (generations >= 100) {
+      // Based on a sample of 10M random patterns 99.84% patterns end before 100 generations.
+      // Save longer patterns for further analysis.
+      ulong64 idx = atomicAdd(numPromising, 1);
+      promising[idx] = pattern;
+      // reset counter ready to advance to next pattern:
+      generations = 0;
+    }
 
-  if ((g1 == g2) || (g1 == g3) || (g1 == g4)) {
-    // periodic
-    generations = 0;
-  }
-
-  return generations;
-}
-
-#ifdef __NVCC__
-__global__ void evaluateRange(ulong64 beginAt, ulong64 endAt,
-                              ulong64 *bestPattern, ulong64 *bestGenerations) {
-  ulong64 patternRange[1024] = {0};
-  for (ulong64 pattern = beginAt; pattern < endAt; pattern += 1024) {
-    int
-    int batchGens = 0;
-    bool active = false;
-    do {
-      for (int i = 0; i < 1024; i++) {
-        if (patternRange[i] >= batchGens) { // pattern hasn't ended
-          active = true; // we've got at least one pattern still running
-          if (patternRange[i] > 48 && threadIdx.x < 64) { // Save long-running patterns for first 64 threads
-            patternRange[i] += countNextSix(beginAt + i);
-          }
-          else {
-            patternRange[i] += countNextSix(beginAt + i);
-          }
-
-          // Save the new longest seen
-          ulong64 old = atomicMax(bestGenerations, patternRange[i]);
-          if (old < patternRange[i]) {
-            *bestPattern = beginAt + i;
-          }
-        }
-      }
-      batchGens += 6;
-    } while (active && genBatch < 50); // Look at 300 generations
-  }
-}
-
-/*
-__global__ void evaluateRange(ulong64 beginAt, ulong64 endAt,
-                              ulong64 *bestPattern, ulong64 *bestGenerations) {
-  for (ulong64 pattern = beginAt + (blockIdx.x * blockDim.x + threadIdx.x);
-       pattern < endAt;
-       pattern += blockDim.x * gridDim.x) {
-    ulong64 generations = countGenerations(pattern);
-    ulong64 old = atomicMax(bestGenerations, generations);
-    if (old < generations) {
-      *bestPattern = pattern;
+    if (generations == 0) {
+      // we reset the generation counter, so load the next pattern:
+      pattern += blockDim.x * gridDim.x;
+      g1 = pattern;
     }
   }
 }
-*/
+
 #endif
 
 __host__ void *search(void *args) {
@@ -238,24 +200,19 @@ __host__ void *search(void *args) {
   }
 
 #ifdef __NVCC__
-  // Allocate memory on CUDA device and locally on host to get the best answers
-  ulong64 *devBestPattern, *hostBestPattern;
-  hostBestPattern = (ulong64 *)malloc(sizeof(ulong64));
-  cudaMalloc((void**)&devBestPattern, sizeof(ulong64));
+  // Allocate memory on CUDA device and locally on host
+  ulong64 *devPromising, *hostPromising;
+  hostPromising = (ulong64 *)malloc(sizeof(ulong64) * 1024 * 1024);
+  cudaMalloc((void**)&devPromising, sizeof(ulong64) * 1024 * 1024);
 
-  ulong64 *devBestGenerations, *hostBestGenerations;
-  hostBestGenerations = (ulong64 *)malloc(sizeof(ulong64));
-  *hostBestGenerations = 0;
-  cudaMalloc((void**)&devBestGenerations, sizeof(ulong64));
+  int *devNumPromising, *hostNumPromising;
+  hostNumPromising = (int *)malloc(sizeof(int));
+  cudaMalloc((void**)&devNumPromising, sizeof(int));
 
   ulong64 chunk = 1;
-  ulong64 chunkSize = 1024*1024; // 1M per call to evaluateRange
+  ulong64 chunkSize = 1024*1024*1024; // 1B patterns (1024 per block per thread)
   ulong64 i = cli->beginAt;
   while (i < cli->endAt) {
-    if (chunk % 1000 == 0) { //
-      printf("."); // every billion patterns
-    }
-
     ulong64 start = i;
     ulong64 end = (start + chunkSize) > cli->endAt ? cli->endAt : start+chunkSize;
     if (cli->random) {
@@ -264,26 +221,29 @@ __host__ void *search(void *args) {
       start = genrand64_int64() % ULONG_MAX;
       end  = start + chunkSize;
     }
-    cudaMemcpy(devBestGenerations, &gBestGenerations, sizeof(ulong64), cudaMemcpyHostToDevice);
-    evaluateRange<<<cli->blockSize, cli->threadsPerBlock>>>(start, end, devBestPattern, devBestGenerations);
 
-    // Copy device answer to host and emit
-    ulong64 prev = *hostBestPattern;
-    cudaMemcpy(hostBestPattern, devBestPattern, sizeof(ulong64), cudaMemcpyDeviceToHost);
-    cudaMemcpy(hostBestGenerations, devBestGenerations, sizeof(ulong64), cudaMemcpyDeviceToHost);
-    if (prev != *hostBestPattern) {
+    // We check ~1B patterns to find any that are longer than 100 generations without simple periods.
+    findPromisingPatterns<<<cli->blockSize, cli->threadsPerBlock>>>(start, end, devPromising, devNumPromising);
+    cudaMemcpy(hostNumPromising, devNumPromising, sizeof(ulong64), cudaMemcpyDeviceToHost);
+    cudaMemcpy(hostPromising, devPromising, *devNumPromising * sizeof(ulong64), cudaMemcpyDeviceToHost);
+
+    // Now iterate over the candidates doing full checks.
+    for (int i = 0; i < *hostNumPromising; i++) {
+      int generations = countGenerations(hostPromising[i]);
       pthread_mutex_lock(&gMutex);
-      if (gBestGenerations <= *hostBestGenerations) {
+      if (gBestGenerations <= generations) {
         char bin[65] = {'\0'};
-        asBinary(*hostBestPattern, bin);
-        printf("[Thread %d] %d generations : %llu : %s\n", cli->threadId, *hostBestGenerations, *hostBestPattern, bin);
-        gBestGenerations = *hostBestGenerations;
+        asBinary(hostPromising[i], bin);
+        printf("[Thread %d] %d generations : %llu : %s\n", cli->threadId, generations, hostPromising[i], bin);
+        gBestGenerations = generations;
       }
       pthread_mutex_unlock(&gMutex);
     }
 
     chunk++;
     i += chunkSize;
+
+    printf("."); // every billion patterns
   }
 #else
   for (ulong64 i = cli->beginAt; i <= cli->endAt; i++) {
