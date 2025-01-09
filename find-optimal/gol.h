ce54ba5e92a0f3710eaf06302ddf0bdee9c64350
aharbick@aharbick.com <aharbick@aharbick.com>
2025-01-08 19:21:02 -0500

    Normalize samples to the chunk that is processed on GPU

--- find-optimal/gol.h
+++ find-optimal/gol.h
@@ -263,7 +263,8 @@ __host__ void searchRandom(prog_args *cli) {
 
   // We're randomly searching..  I didn't get cuRAND to work so we randomize our batches. Each
   // call to findCandidates is sequential but we look at random locations across all possible.
-  for (ulong64 i = 0; i < cli->randomSamples; i++) {
+  ulong64 iterations = cli->randomSamples / CHUNK_SIZE;
+  for (ulong64 i = 0; i < iterations; i++) {
     ulong64 start = genrand64_int64();
     ulong64 end = start + CHUNK_SIZE;
 
